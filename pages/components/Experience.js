b0a2dd2ad0d79fdd12d367739e51358e8f210038
Jay WInebrenner <jaywinebrenner@gmail.com>
2022-03-21 10:54:53 -0700

    clean up mobile views begin exp section

---
+++ pages/components/Experience.js
@@ -0,0 +1,29 @@
+
+
+import React, {useRef, useEffect} from 'react'
+
+const Experience = () => {
+
+
+
+    useEffect(() => {
+      }, [])
+
+
+  return (
+    <div className="experience">
+    
+        <div className="experience__top-left">
+            <h1>DEV</h1>
+            <h2>ELOPMENT EXPERIENCE</h2>
+        </div>
+        <div className="experience__top-right">
+            
+        </div>
+    
+   
+    </div>
+  );
+};
+
+export default Experience;
