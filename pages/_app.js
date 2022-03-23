d11e2262de3c128b1bf564f2261a8e9019a29144
Jay WInebrenner <jaywinebrenner@gmail.com>
2022-03-23 09:33:42 -0700

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 
 import "../styles/app.scss";
 import { LoadingProvider } from "./context/LoadingContext";
+import 'nprogress/nprogress.css';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+
+  Router.events.on("routeChangeStart", () =>
+  NProgress.start()
+);
+Router.events.on("routeChangeComplete", () =>
+  NProgress.done()
+);
+Router.events.on("routeChangeError", () =>
+  NProgress.done()
+);
   return (  
     <LoadingProvider>
         <Component {...pageProps} />
